4b3dd1e6e8f0cfb6bf73fec171333a5b4566a2b0
Shivam gond <codewithharry35434@gmail.com>
2025-07-15 16:35:01 +0530

    Implemented student and course portal with CRUD-like GET routes and custom 404 handler

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-// server.js
-
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON
+// Routers
+const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+
+// Middleware
 app.use(express.json());
 
-// Import routes
-const bookRoutes = require('./routes/bookRoutes');
+// Home Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Student & Course Portal API!');
+});
 
-// Use book router
-app.use('/', bookRoutes);
+// Student Routes
+app.use('/students', studentRoutes);
 
-// Start the server
+// Course Routes
+app.use('/courses', courseRoutes);
+
+// 404 Handler (Wildcard)
+app.use('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
+
+// Start Server
 app.listen(4000, () => {
-  console.log('📚 Library server running at http://localhost:4000');
+  console.log('🚀 Server running on http://localhost:4000');
 });
